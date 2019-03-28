8eb5d13157ba6e80a279c632f07f8c6411e70de2
Kajachuan <kevincajachuan@hotmail.com>
2019-03-28 14:40:57 -0300

    Configuracion de mongo

--- app/app.py
+++ app/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+from os import environ
+
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'hypechat',
+    'host': environ['MONGODB_URI']
+}
+db = MongoEngine(app)
 
 @app.route('/')
 def hello_world():
+    from .models.user import User
+    
+    u = User(name='Gino')
+    u.save()
     return 'Hello, World!'
