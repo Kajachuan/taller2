33639280db104c26c865d64c0c59b49078e5629d
Kajachuan <kevincajachuan@hotmail.com>
2019-04-29 17:06:09 -0300

    Envio response en el login y logout

--- tests/app/controllers/test_sessions.py
+++ tests/app/controllers/test_sessions.py
@@ -10,19 +10,23 @@ class TestSessionsController(object):
         response = client.post('/login', data='{"username": "testlogin", "password": "mipass"}')
 
         assert response.status_code == HTTPStatus.OK
+        assert response.get_json() == {'message': 'The user testlogin is logged in'}
 
     def test_wrong_user(self):
         response = client.post('/login', data='{"username": "cualquiera", "password": "mipass"}')
 
         assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.get_json() == {'message': 'The username or password are wrong'}
 
     def test_wrong_password(self):
         response = client.post('/login', data='{"username": "testlogin", "password": "malpass"}')
 
         assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.get_json() == {'message': 'The username or password are wrong'}
 
     def test_correct_logout(self):
         client.post('/login', data='{"username": "testlogin", "password": "mipass"}')
         response = client.delete('/logout')
 
         assert response.status_code == HTTPStatus.OK
+        assert response.get_json() == {'message': 'The user testlogin was logged out'}
