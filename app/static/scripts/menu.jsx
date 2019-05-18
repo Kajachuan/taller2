6c9b3f624f28cb8a4384ff5732790cbe1e1f5dd8
Kajachuan <kevincajachuan@hotmail.com>
2019-05-18 18:13:49 -0300

    Estilo en el menú

--- app/static/scripts/menu.jsx
+++ app/static/scripts/menu.jsx
@@ -14,6 +14,7 @@ class MenuButton extends React.Component {
     return(
       <button class="menu-button">
         <img src={this.state.img} height="50" width="50" />
+        <br/>
         {this.state.name}
       </button>
     );
@@ -23,7 +24,8 @@ class MenuButton extends React.Component {
 class Menu extends React.Component {
   render() {
     return(
-      <div>
+      <div class="menu">
+        <h1>Hypechat Back Office</h1>
         <MenuButton name="Administración de Equipos" img="/static/img/organization.png" />
         <MenuButton name="Administración de Usuarios" img="/static/img/user.png" />
         <MenuButton name="Administración de Palabras Prohibidas" img="/static/img/words.png" />
