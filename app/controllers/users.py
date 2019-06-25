00df74b58717a1585088b88fa84ae5b382de77df
Kajachuan <kevincajachuan@hotmail.com>
2019-06-25 11:27:56 -0300

    Cambio formato del diccionario en el perfil

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -75,11 +75,11 @@ def get_profile(username):
     for organization_name in user.organizations:
         organization = Organization.objects.get(organization_name=organization_name)
         if user == organization.owner:
-            organizations[organization_name] = {'rol': 'owner'}
+            organizations[organization_name] = {'rol': {'rol': 'owner'}}
         elif user in organization.moderators:
-            organizations[organization_name] = {'rol': 'moderator'}
+            organizations[organization_name] = {'rol': {'rol': 'moderator'}}
         else:
-            organizations[organization_name] = {'rol': 'member'}
+            organizations[organization_name] = {'rol': {'rol': 'member'}}
 
         for channel in organization.channels:
             if user.username == channel.owner:
