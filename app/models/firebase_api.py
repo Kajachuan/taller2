1f19ee6f746242c389904f7dc0908619f99df7cf
guille <guillecondori19@gmail.com>
2019-06-27 01:14:56 -0300

    fix firebase default app

--- app/models/firebase_api.py
+++ app/models/firebase_api.py
@@ -18,11 +18,14 @@ HELP_KEYS = ['help', 'info', 'mute', 'me']
 INFO_KEYS = ['name', 'owner', 'description']
 ME_KEYS = ['username', 'first_name', 'last_name', 'email']
 
+default_app = None
+
 class FirebaseApi(object):
     def __init__(self):
         if environ['FLASK_ENV'] == PRODUCTION:
-            cred = credentials.Certificate(CREDENTIALS_PATH)
-            self.default_app  = firebase_admin.initialize_app(cred)
+            if not default_app:
+                cred = credentials.Certificate(CREDENTIALS_PATH)
+                default_app  = firebase_admin.initialize_app(cred)
 
     def send_message_to_users(self, list_username, message, organization_name, channel_name):
         if environ['FLASK_ENV'] != PRODUCTION:
