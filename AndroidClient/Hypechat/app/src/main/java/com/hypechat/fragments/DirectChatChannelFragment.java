e7084bfb0dfd45e54e9da657cceb590d6978532c
Fabrizio <fabrizio.cozza@gmail.com>
2019-07-03 23:00:28 -0300

    Imagenes en conversaciones

--- AndroidClient/Hypechat/app/src/main/java/com/hypechat/fragments/DirectChatChannelFragment.java
+++ AndroidClient/Hypechat/app/src/main/java/com/hypechat/fragments/DirectChatChannelFragment.java
@@ -393,10 +393,10 @@ public class DirectChatChannelFragment extends Fragment {
     }
 
     public String bitmapToString(Bitmap bitmap){
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-        byte[] byteArray = byteArrayOutputStream .toByteArray();
-        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        return Base64.encodeToString(b, Base64.URL_SAFE);
     }
 
     @Override
